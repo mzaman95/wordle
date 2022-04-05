454dfc78f8debca5c67ceaafeefac49fd3c709f2
Mridula Zaman <mridula.zaman95@gmail.com>
2022-04-04 23:03:15 -0700

    update features for wordle

--- script.js
+++ script.js
@@ -1,51 +1,62 @@
 window.onload = () => {
-  let winnerText = document.getElementById("winnerText");
-  let errorText = document.getElementById("errorText");
+  let debugButton = document.getElementById("debug");
   let newGameBtn = document.getElementById("newGame");
   newGameBtn.onclick = function (event) {
     window.location.reload();
     event.preventDefault();
   };
+  let correctAnswer = document.getElementById("correctAnswer");
+  let bannerMessage = document.getElementById("bannerMessage");
+  debugButton.addEventListener("click", function () {
+    if (debugButton.innerHTML == "Debug Mode: Off") {
+      debugButton.innerHTML = "Debug Mode: On";
+      correctAnswer.innerHTML = "Correct Answer: " + word;
+    } else {
+      debugButton.innerHTML = "Debug Mode: Off";
+      correctAnswer.innerHTML = "";
+    }
+  });
 
-  const letters = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
+  let letters = [
+    { letter: "A", state: "unused" },
+    { letter: "B", state: "unused" },
+    { letter: "C", state: "unused" },
+    { letter: "D", state: "unused" },
+    { letter: "E", state: "unused" },
+    { letter: "F", state: "unused" },
+    { letter: "G", state: "unused" },
+    { letter: "H", state: "unused" },
+    { letter: "I", state: "unused" },
+    { letter: "J", state: "unused" },
+    { letter: "K", state: "unused" },
+    { letter: "L", state: "unused" },
+    { letter: "M", state: "unused" },
+    { letter: "N", state: "unused" },
+    { letter: "O", state: "unused" },
+    { letter: "P", state: "unused" },
+    { letter: "Q", state: "unused" },
+    { letter: "R", state: "unused" },
+    { letter: "S", state: "unused" },
+    { letter: "T", state: "unused" },
+    { letter: "U", state: "unused" },
+    { letter: "V", state: "unused" },
+    { letter: "W", state: "unused" },
+    { letter: "X", state: "unused" },
+    { letter: "Y", state: "unused" },
+    { letter: "Z", state: "unused" },
   ];
+
   let letterList = document.getElementById("letterList");
   for (let i = 0; i < letters.length; i++) {
-    let letterValue = letters[i];
+    let letterValue = letters[i].letter;
+    let state = letters[i].state;
     let letter = document.createElement("span");
-    letter.setAttribute("class", "positionNextTo letter default");
+    letter.setAttribute("class", "letter " + state);
     letter.setAttribute("id", letterValue);
     letter.innerHTML = letterValue;
     letterList.appendChild(letter);
   }
 
-  // Utilizing XMLHttpRequest to Random Word API w/ param for 5 letter word
   const data = null;
   let word = null;
 
@@ -73,32 +84,19 @@ window.onload = () => {
     word = "light".toUpperCase();
   }
 
-  getNewWord();
-
-  let debugModeButton = document.getElementById("debugModeButton");
-  let correctAnswer = document.getElementById("correctAnswer");
-  debugModeButton.addEventListener("click", function () {
-    if (debugModeButton.innerHTML == "Debug Mode: Off") {
-      debugModeButton.innerHTML = "Debug Mode: On";
-      correctAnswer.innerHTML = "Correct Answer: " + word;
-    } else {
-      debugModeButton.innerHTML = "Debug Mode: Off";
-      correctAnswer.innerHTML = "";
-    }
-  });
-
   function checkWordExists(word) {
+    let message = document.getElementById("guessMessage" + guessNumber);
     const request = new XMLHttpRequest();
     request.withCredentials = false;
     request.addEventListener("readystatechange", function () {
       if (this.readyState === this.DONE) {
         if (this.status != 200) {
-          errorText.setAttribute("class", "messageBanner");
-          /*
-          alert(
-            "The guess you entered is not a word. Please enter a different guess."
-          );
-          */
+          message.innerHTML =
+            "That's not a real word. Please try a different word.";
+        }
+        if (this.status == 200) {
+          message.innerHTML = "";
+          assessGuess(word);
         }
       }
     });
@@ -110,74 +108,92 @@ window.onload = () => {
     request.send(data);
   }
 
-  console.log(word);
+  function checkIfCorrectLetter(guessedLetter, wordArray) {
+    for (let i = 0; i < wordArray.length; i++) {
+      if (guessedLetter == wordArray[i]) return true;
+    }
+    return false;
+  }
 
   function assessGuess(guess) {
-    checkWordExists(guess);
-    let guessArray = guess.split("");
+    let coloredLettersElement = document.getElementById(
+      "coloredLetters" + guessNumber
+    );
+    let guessArray = guess.toUpperCase().split("");
     let wordArray = word.split("");
-    if (guess.toUpperCase() == word) {
-      winnerText.setAttribute("class", "messageBanner");
+
+    if (guess.toUpperCase() == word.toUpperCase()) {
+      bannerMessage.setAttribute("class", "won");
+      bannerMessage.innerHTML = "Congrats! You figured out the word!";
     } else {
-      for (let i = 0; i < 5; i++) {
-        if (guessArray[i].toUpperCase() == wordArray[i]) {
-          let letter = document.getElementById(guessArray[i].toUpperCase());
-          letter.setAttribute(
-            "class",
-            "positionNextTo letter correctPlacement"
-          );
+      for (let i = 0; i < guessArray.length; i++) {
+        let singleLetter = document.createElement("span");
+        singleLetter.innerHTML = guessArray[i];
+        let letter = document.getElementById(guessArray[i]);
+        letter.setAttribute("class", "letter used");
+        if (guessArray[i] == wordArray[i]) {
+          singleLetter.setAttribute("class", "letter correctPlacement");
         } else {
-          for (let j = 0; j < 5; j++) {
-            if (guessArray[i].toUpperCase() == wordArray[j]) {
-              let letter = document.getElementById(guessArray[i].toUpperCase());
-              letter.setAttribute(
-                "class",
-                "positionNextTo letter correctLetter"
-              );
-            } else {
-              let letter = document.getElementById(guessArray[i].toUpperCase());
-              letter.setAttribute("class", "positionNextTo letter used");
-            }
-          }
+          let isFound = checkIfCorrectLetter(guessArray[i], wordArray);
+          if (isFound) {
+            singleLetter.setAttribute("class", "letter correctLetter");
+          } else singleLetter.setAttribute("class", "letter used");
         }
+        coloredLettersElement.appendChild(singleLetter);
+      }
+      if (guessNumber < 6) {
+        guessNumber += 1;
+        createGuessButton(guessNumber);
+      } else if ((guessNumber = 6)) {
+        bannerMessage.setAttribute("class", "lost");
+        bannerMessage.innerHTML = "Sorry, you lost!";
       }
     }
   }
 
+  getNewWord();
+
+  let guessNumber = 1;
   guessesArray = [];
   let guessessArrayDiv = document.getElementById("guessesArray");
-  for (let i = 0; i < 6; i++) {
+
+  const createGuessButton = (guessNumber) => {
     let guessForm = document.createElement("form");
-    guessForm.setAttribute("id", "guessForm" + i);
+    guessForm.setAttribute("id", "guessForm" + guessNumber);
     guessessArrayDiv.appendChild(guessForm);
 
     let guessLabel = document.createElement("label");
-    guessLabel.setAttribute("for", "guess" + i);
-    guessLabel.innerHTML = "Guess " + (i + 1) + ": ";
+    guessLabel.setAttribute("for", "guess" + guessNumber);
+    guessLabel.innerHTML = "Guess " + guessNumber + ":  &nbsp;";
     guessForm.appendChild(guessLabel);
 
     let guessInput = document.createElement("input");
     guessInput.setAttribute("type", "text");
-    guessInput.setAttribute("id", "guess" + i);
-    guessInput.setAttribute("name", "guess" + i);
+    guessInput.setAttribute("id", "guess" + guessNumber);
+    guessInput.setAttribute("name", "guess" + guessNumber);
     guessInput.setAttribute("maxlength", "5");
     guessInput.setAttribute("value", "");
     guessForm.appendChild(guessInput);
 
     let guessButton = document.createElement("button");
     guessButton.setAttribute("type", "button");
-    guessButton.setAttribute("id", "guessButton" + i);
+    guessButton.setAttribute("id", "guessButton" + guessNumber);
     guessButton.innerHTML = "Submit";
     guessForm.appendChild(guessButton);
 
     guessButton.onclick = function (event) {
-      assessGuess(guessInput.value.toLowerCase());
-      guessInput.setAttribute("disabled", "true");
-      guessButton.setAttribute("disabled", "true");
+      checkWordExists(guessInput.value.toLowerCase());
       event.preventDefault();
     };
 
-    let br = document.createElement("span");
-    guessForm.appendChild(br);
-  }
+    let guessMessage = document.createElement("span");
+    guessMessage.setAttribute("id", "guessMessage" + guessNumber);
+    guessForm.appendChild(guessMessage);
+
+    let coloredLetters = document.createElement("span");
+    coloredLetters.setAttribute("id", "coloredLetters" + guessNumber);
+    guessForm.appendChild(coloredLetters);
+  };
+
+  createGuessButton(1);
 };
